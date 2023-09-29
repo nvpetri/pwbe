3622dc0a38c76b76be36275aa9c6c0f5677c6da3
NPS <nicolaspetri18@gmail.com>
2023-09-29 09:03:39 -0300

    iniciando aula 7 Array e JSON

---
+++ aula7/modules/array_json.js
@@ -0,0 +1,9 @@
+/*****************************************************************
+ * Autor: Nicolas Vasconcelos
+ * Data: 29/09/2023
+ * Versão: 1.0
+ * Objetivo: Trabalhar com estrutura de dados em Array e JSON
+ *****************************************************************/
+
+// [ ] -> Representa a estrutura array
+// { } -> Representa a estrutura json
