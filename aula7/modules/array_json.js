ca690430867099904aaf85a38518c338f6f4fab9
NPS <nicolaspetri18@gmail.com>
2023-09-29 09:31:18 -0300

    array

--- aula7/modules/array_json.js
+++ aula7/modules/array_json.js
@@ -7,3 +7,27 @@
 
 // [ ] -> Representa a estrutura array
 // { } -> Representa a estrutura json
+
+const introducaoArray = function() {
+
+    // criando um array de forma manual
+    let listaDeNomes = ['José', 'Ana', 'Luiz', 'Maria', 'Carlos', 'Eduardo']
+
+    // exibe os dados do array
+    console.log(listaDeNomes)
+
+    //exibe os dados do array em forma de tabela
+    console.table(listaDeNomes)
+
+    // retorna a quantidade de elementos de um array
+
+    console.log(listaDeNomes.length)
+
+    // Adiciona elementos novos no final do array
+    listaDeNomes.push('Roberto')
+
+    console.table(listaDeNomes)
+
+}
+
+introducaoArray()
